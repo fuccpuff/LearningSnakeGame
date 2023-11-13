eae9e8e07508d1739efb3952bd7b0883b50f780c
LarionovPS <LarionovPS@DESKTOP-QNB1STK>
2023-11-13 10:15:01 +0500

    Добавьте файлы проекта.

---
+++ SnakeGame/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbCanvas = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbCanvas
+            // 
+            this.pbCanvas.Location = new System.Drawing.Point(11, 11);
+            this.pbCanvas.Name = "pbCanvas";
+            this.pbCanvas.Size = new System.Drawing.Size(657, 426);
+            this.pbCanvas.TabIndex = 0;
+            this.pbCanvas.TabStop = false;
+            this.pbCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.pbCanvas_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(675, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbCanvas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pbCanvas;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+    }
+}
---
+++ SnakeGame/Form1.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> snake = new List<Point>();
+        private Point food = new Point();
+        private Direction direction = Direction.Down;
+        private int maxWidth;
+        private int maxHeight;
+        private int score;
+        private bool gameOver;
+
+        private System.Windows.Forms.Timer gameTimer = new System.Windows.Forms.Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            maxWidth = pbCanvas.Width / 10; // Задаем размер клетки 10x10
+            maxHeight = pbCanvas.Height / 10;
+            snake.Clear();
+            snake.Add(new Point(10, 5)); // Начальное положение змейки
+            score = 0;
+            gameOver = false;
+            direction = Direction.Down;
+            gameTimer.Interval = 100; // Скорость игры
+            gameTimer.Tick += Update;
+            gameTimer.Start();
+            GenerateFood();
+        }
+
+        private void GenerateFood()
+        {
+            Random random = new Random();
+            food = new Point(random.Next(0, maxWidth), random.Next(0, maxHeight));
+        }
+
+        private void Update(object sender, EventArgs e)
+        {
+            if (gameOver)
+            {
+                gameTimer.Stop();
+                if (MessageBox.Show("Игра окончена. Хотите начать снова?", "Змейка", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    InitializeGame();
+                }
+                else
+                {
+                    Close();
+                }
+            }
+            MoveSnake();
+            pbCanvas.Invalidate(); // Перерисовка PictureBox
+        }
+
+        private void MoveSnake()
+        {
+            for (int i = snake.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    switch (direction)
+                    {
+                        case Direction.Up:
+                            snake[i] = new Point(snake[i].X, snake[i].Y - 1);
+                            break;
+                        case Direction.Down:
+                            snake[i] = new Point(snake[i].X, snake[i].Y + 1);
+                            break;
+                        case Direction.Left:
+                            snake[i] = new Point(snake[i].X - 1, snake[i].Y);
+                            break;
+                        case Direction.Right:
+                            snake[i] = new Point(snake[i].X + 1, snake[i].Y);
+                            break;
+                    }
+
+                    // Проверка столкновения с границами
+                    if (snake[i].X < 0 || snake[i].Y < 0 || snake[i].X >= maxWidth || snake[i].Y >= maxHeight)
+                    {
+                        gameOver = true;
+                    }
+
+                    // Проверка столкновения с телом змейки
+                    for (int j = 1; j < snake.Count; j++)
+                    {
+                        if (snake[i].Equals(snake[j]))
+                        {
+                            gameOver = true;
+                        }
+                    }
+
+                    // Проверка столкновения с едой
+                    if (snake[i].Equals(food))
+                    {
+                        EatFood();
+                    }
+                }
+                else
+                {
+                    snake[i] = snake[i - 1];
+                }
+            }
+        }
+
+        private void EatFood()
+        {
+            Point tail = snake[snake.Count - 1];
+            snake.Add(new Point(tail.X, tail.Y));
+            score += 10; // Увеличение счета
+            GenerateFood();
+        }
+
+        private void pbCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+            if (!gameOver)
+            {
+                // Отрисовка змейки
+                for (int i = 0; i < snake.Count; i++)
+                {
+                    Brush snakeColor = i == 0 ? Brushes.Black : Brushes.Green;
+                    canvas.FillRectangle(snakeColor, new Rectangle(snake[i].X * 10, snake[i].Y * 10, 10, 10));
+                }
+                // Отрисовка еды
+                canvas.FillRectangle(Brushes.Red, new Rectangle(food.X * 10, food.Y * 10, 10, 10));
+            }
+            else
+            {
+                string gameOverText = $"Игра окончена\nВаш счет: {score}";
+                Label lblGameOver = new Label();
+                lblGameOver.Text = gameOverText;
+                lblGameOver.Size = new Size(200, 70);
+                lblGameOver.TextAlign = ContentAlignment.MiddleCenter;
+                pbCanvas.Controls.Add(lblGameOver);
+                lblGameOver.Location = new Point((pbCanvas.Width - lblGameOver.Width) / 2, (pbCanvas.Height - lblGameOver.Height) / 2);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    direction = Direction.Up;
+                    break;
+                case Keys.Down:
+                    direction = Direction.Down;
+                    break;
+                case Keys.Left:
+                    direction = Direction.Left;
+                    break;
+                case Keys.Right:
+                    direction = Direction.Right;
+                    break;
+            }
+        }
+    }
+
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+}
